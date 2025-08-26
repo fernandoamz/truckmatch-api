e592f7c75d67465d2781a9caabd5ce9ff63d2936
Fernando Amezcua <fernandoamz148@gmail.com>
2025-08-26 17:21:32 -0600

    Add graphql/types/unit.js 1756250492002

---
+++ graphql/types/unit.js
@@ -0,0 +1,13 @@
+const { GraphQLObjectType, GraphQLID, GraphQLString } = require('graphql');
+
+const UnitType = new GraphQLObjectType({
+  name: 'Unit',
+  fields: () => ({
+    id: { type: GraphQLID },
+    plateNumber: { type: GraphQLString },
+    model: { type: GraphQLString },
+    userId: { type: GraphQLID }
+  })
+});
+
+module.exports = UnitType;
